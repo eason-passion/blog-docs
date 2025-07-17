39c757b4227f1e8ccbc6102f853bb7e882f89c3f
YHH <1371356187@qq.com>
2025-07-17 09:37:33 +0800

    add

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -35,7 +35,7 @@ export default defineConfig({
     },
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/eason-passion' }
     ],
     footer: {
       message: 'Released under the MIT License.',
